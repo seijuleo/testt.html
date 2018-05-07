63faeb78158320927b7685118e8410cb005e101f
seijuleo <39026792+seijuleo@users.noreply.github.com>
2018-05-07 19:50:12 +0800

    Add files via upload

--- mian.js
+++ mian.js
@@ -4,9 +4,6 @@ function check(){
 	var question1 = document.quiz.question1.value;
 	var question2 = document.quiz.question2.value;
 	var question3 = document.quiz.question3.value;
-	var question4 = document.quiz.question4.value;
-	var question5 = document.quiz.question5.value;
-	var question6 = document.quiz.question6.value;
 	var correct = 0;
 
 
@@ -18,36 +15,37 @@ function check(){
 }	
 	if (question3 == "tissues") {
 		correct++;
-}
+	
 	if (question4 == "blood cells") {
 		correct++;
-}	
+	
 	if (question5 == "involuntary") {
 		correct++;
-}	
+	
 	if (question6 == "voluntary") {
 		correct++;
-    }
-	var pictures = ["wni.gif", "ok.gif", "sad.gif"];
+	
+	}
+	
+	var pictures = ["img/win.gif", "img/meh.jpeg", "img/lose.gif"];
 	var messages = ["Great job!", "That's just okay", "You really need to do better"];
-	
-    var range;
-	
-	if (correct < 2) {
-		range = 2;
+	var score;
+
+	if (correct == 0) {
+		score = 2;
 	}
 
-	if (correct > 2 && correct < 5) {
-		range = 1;
+	if (correct > 0 && correct < 3) {
+		score = 1;
 	}
 
-	if (correct > 5) {
-		range = 0;
+	if (correct == 3) {
+		score = 0;
 	}
 
 	document.getElementById("after_submit").style.visibility = "visible";
 
-	document.getElementById("message").innerHTML = messages[range];
+	document.getElementById("message").innerHTML = messages[score];
 	document.getElementById("number_correct").innerHTML = "You got " + correct + " correct.";
-	document.getElementById("picture").src = pictures[range];
+	document.getElementById("picture").src = pictures[score];
 	}
